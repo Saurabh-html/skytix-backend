312ad762b88cda1c8e5ef5b6d65b749e551cd1af
Saurabh Suman <saurabhsuman843325@gmail.com>
2026-03-24 19:59:43 +0530

    Modified user profile

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,3 +1,6 @@
+const express = require('express');
+const router = express.Router(); 
+
 const {
   registerUser,
   loginUser,
@@ -8,11 +11,19 @@ const {
 
 const { protect } = require('../middleware/authMiddleware');
 
+// REGISTER
 router.post('/register', registerUser);
+
+// LOGIN
 router.post('/login', loginUser);
 
+// GET PROFILE
 router.get('/profile', protect, getUserProfile);
 
+// UPDATE PROFILE (NEW)
 router.put('/profile', protect, updateUserProfile);
 
+// RESET PASSWORD
 router.put('/reset-password', resetPassword);
+
+module.exports = router;
